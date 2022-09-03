88aa07c4cc35fb9eccb37e8a622d9e67b1272bdd
Stefan Dunikovskyi <stefan090304@gmail.com>
2022-09-03 11:58:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import './index.scss';
+import "./index.scss";
+import { useReducer } from "react";
 
 function App() {
+  const [count, dispatch] = useReducer(onClickFunc, 0);
+
+  function onClickFunc(state, action) {
+    switch (action.type) {
+      case "increment":
+        return state + action.payload;
+      case "decrement":
+        return state - action.payload;
+      default:
+        return state;
+    }
+  }
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button
+          className="minus"
+          onClick={() => dispatch({ type: "decrement", payload: 1 })}
+        >
+          - Минус
+        </button>
+        <button
+          className="plus"
+          onClick={() => dispatch({ type: "increment", payload: 1 })}
+        >
+          Плюс +
+        </button>
       </div>
     </div>
   );
